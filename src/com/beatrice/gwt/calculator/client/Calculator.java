5e390f2228d64bc963c6ed6ee685df3d4bf9dc26
bjarlung <beatrice.jarlung@gmail.com>
2017-08-23 10:48:28 +0200

    style and bugfix

--- src/com/beatrice/gwt/calculator/client/Calculator.java
+++ src/com/beatrice/gwt/calculator/client/Calculator.java
@@ -2,7 +2,6 @@ package com.beatrice.gwt.calculator.client;
 
 import java.util.ArrayList;
 
-import org.eclipse.jdt.internal.core.NameLookup.Answer;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dev.javac.testing.Source;
@@ -28,67 +27,95 @@ public class Calculator implements EntryPoint {
 
 	private ArrayList<Button> numberButtonList;
 	private VerticalPanel mainPanel = new VerticalPanel();
-	private HorizontalPanel numberPanel;
+	private HorizontalPanel content;
 	private HorizontalPanel displayPanel;
+	private HorizontalPanel numberPanel1;
+	private HorizontalPanel numberPanel2;
 	private HorizontalPanel operatorPanel;
+	private HorizontalPanel calcPanel;
 	private Label operand1Label;
 	private Label operand2Label;
 	private Label operatorLabel;
 	private Label answerLabel;
 	private Label activeOperand;
 	private Button calculateButton;
-	private FlexTable resultsTable = new FlexTable();
+	private FlexTable resultsTable;
 	private static final String OPERATOR_STRING = "+-*/%";
 	
 	
 	/**
 	 * This is the entry point method.
 	 */
-	public void onModuleLoad() {
+	public void onModuleLoad() {	
 		
-		numberButtonList = new ArrayList<>();
-		numberPanel = new HorizontalPanel();
-		numberPanel.addStyleName("numberPanel");
-		displayPanel = new HorizontalPanel();
+		content = new HorizontalPanel();
+		
 		operatorPanel = new HorizontalPanel();
-		mainPanel = new VerticalPanel();
-		answerLabel = new Label();
-		operand1Label = new Label();
-		operand2Label = new Label();
-		operatorLabel = new Label();
-		activeOperand = operand1Label;
+		
+		calcPanel = new HorizontalPanel();
+		mainPanel = new VerticalPanel();	
+		
 		calculateButton = new Button("Calculate");
-		calculateButton.addStyleName("calcButton");
-		
-		displayPanel.add(operand1Label);
-		displayPanel.add(operatorLabel);
-		displayPanel.add(operand2Label);
-		
-		setUpNumberPanel();
-		setUpOperatorPanel();
+		resultsTable = new FlexTable();
+		resultsTable.addStyleName("resultsTable");
+		resultsTable.getRowFormatter().addStyleName(0, "resultsHeader");
 		resultsTable.setText(0, 0, "Problem");
 		resultsTable.setText(0, 2, "Answer");
+		
+		setUpDisplays();
+		setUpNumberPanel();
+		setUpOperatorPanel();
+		
+		calculateButton.addStyleName("calcButton");
+		calculateButton.setSize("200px", "50px");
+		calcPanel.add(calculateButton);
 
+		mainPanel.addStyleName("mainContent");
+		
 		mainPanel.add(displayPanel);
-		mainPanel.add(numberPanel);
+		mainPanel.add(numberPanel1);
+		mainPanel.add(numberPanel2);
 		mainPanel.add(operatorPanel);
-		mainPanel.add(calculateButton);
+		mainPanel.add(calcPanel);
 		mainPanel.add(answerLabel);
-		mainPanel.add(resultsTable);
+		
+		content.add(mainPanel);
+		content.add(resultsTable);
 
 		// Associate the Number panel with the HTML host page.
-		RootPanel.get("numberButtonContainer").add(mainPanel);
+		RootPanel.get("numberButtonContainer").add(content);
 
 		calculateButton.addClickHandler(e -> calculate());
 
 	}
 	
+	private void setUpDisplays() {
+		
+		answerLabel = new Label("");
+		operand1Label = new Label("");
+		operand2Label = new Label("");
+		operatorLabel = new Label("");
+		answerLabel.setHeight("40px");
+		operand1Label.setHeight("40px");
+		operand2Label.setHeight("40px");
+		operatorLabel.setHeight("40px");
+		
+		activeOperand = operand1Label;
+		displayPanel = new HorizontalPanel();
+		
+		displayPanel.add(operand1Label);
+		displayPanel.add(operatorLabel);
+		displayPanel.add(operand2Label);
+		
+	}
+
 	private void setUpOperatorPanel() {
 		
 		for(char ch: OPERATOR_STRING.toCharArray()) {
 			Button button = new Button(Character.toString(ch));
 			operatorPanel.add(button);
 			button.addStyleName("numberButton");
+			button.setSize("50px", "50px");
 			button.setEnabled(true);
 			//button.setFocus(true);
 			button.addClickHandler(e -> addOperator(e));
@@ -102,6 +129,7 @@ public class Calculator implements EntryPoint {
 		}
 		Button sourceButton = (Button)e.getSource();
 		String operator = sourceButton.getText();
+		
 		if(activeOperand == operand1Label && activeOperand.getText() != "") {
 			operatorLabel.setText(operator);
 			activeOperand = operand2Label;
@@ -116,17 +144,28 @@ public class Calculator implements EntryPoint {
 	private boolean enterPressed = false;
 	
 	private void setUpNumberPanel() {
+		numberButtonList = new ArrayList<>();
+		numberPanel1 = new HorizontalPanel();
+		numberPanel1.addStyleName("numberPanel");
+		numberPanel2 = new HorizontalPanel();
+		numberPanel2.addStyleName("numberPanel");
+		
 		for (int i = 0; i<10; i++) {
-			setUpNumberButton(Integer.toString(i));
+			Button button = setUpNumberButton(Integer.toString(i));
+			if(i<5)
+				numberPanel1.add(button);
+			else
+				numberPanel2.add(button);
 		}
-		setUpNumberButton(".");
+		calcPanel.add(setUpNumberButton("."));
 	}
 
-	private void setUpNumberButton(String buttonText) {
+	private Button setUpNumberButton(String buttonText) {
 		Button button = new Button(buttonText);
-		numberButtonList.add(button);
-		numberPanel.add(button);
+		//numberButtonList.add(button);
+		
 		button.addStyleName("numberButton");
+		button.setSize("50px", "50px");
 		button.setEnabled(true);
 		button.setFocus(true);
 		button.addClickHandler(e -> addToOperand(e));
@@ -136,13 +175,18 @@ public class Calculator implements EntryPoint {
 	    		calculate();
 	    	}
 	    });
+		return button;
 	}
 
 	private void addToOperand(ClickEvent e) {
-		Button sourceButton = (Button)e.getSource();
-		String operandTextString = activeOperand.getText();
-		String buttonText = sourceButton.getText();
-		activeOperand.setText(operandTextString + buttonText);
+		if(activeOperand.getText().length()>10) {
+			Window.alert("This program does not support numbers over 10 characters");
+		} else {
+			Button sourceButton = (Button)e.getSource();
+			String operandTextString = activeOperand.getText();
+			String buttonText = sourceButton.getText();
+			activeOperand.setText(operandTextString + buttonText);
+		}
 	}
 
 	//_______________________________________________________________________
@@ -156,12 +200,26 @@ public class Calculator implements EntryPoint {
 		if(operator.equals("*")) {
 			answer = multiply(operand1, operand2);
 		} else if (operator.equals("%")) {
-			answer = modulo(operand1, operand2);			 
+			try {
+				answer = modulo(operand1, operand2);
+			} catch (IllegalOperandException e) {
+				Window.alert(e.getMessage()+ " not supported");
+				e.printStackTrace();
+				resetOperands();
+				return;
+			}			 
 		} else if (operator.equals("/")) {
-			answer = divide(operand1, operand2);	 
+			try {
+				answer = divide(operand1, operand2);
+			} catch (IllegalOperandException e) {
+				Window.alert(e.getMessage()+ " not supported");
+				e.printStackTrace();
+				resetOperands();
+				return;
+			}	 
 		} else if (operator.equals("-")) {
 			answer = subtract(operand1, operand2);			 
-		} else {   							 //addition
+		} else {   							 
 			answer = add(operand1, operand2);				
 		}
 		
@@ -180,12 +238,18 @@ public class Calculator implements EntryPoint {
 		return operand1 - operand2;
 	}
 
-	private double modulo(double operand1, double operand2) {
-		return operand1 % operand2;
+	private double modulo(double operand1, double operand2)  throws IllegalOperandException{
+		if(operand2 == 0)
+			throw new IllegalOperandException("Modulo of 0");
+		else
+			return operand1 % operand2;
 	}
 
-	private double divide(double operand1, double operand2) {
-		return operand1 / operand2;
+	private double divide(double operand1, double operand2) throws IllegalOperandException{
+		if(operand2 == 0)
+			throw new IllegalOperandException("Division by 0");
+		else
+			return operand1 / operand2;
 	}
 
 	private double multiply(double operand1, double operand2) {
---
+++ src/com/beatrice/gwt/calculator/client/IllegalOperandException.java
@@ -0,0 +1,14 @@
+package com.beatrice.gwt.calculator.client;
+
+
+public class IllegalOperandException extends Exception {
+	
+	public IllegalOperandException() {
+		super();
+	}
+	public IllegalOperandException(String message) {
+		super(message);
+	}
+	
+
+}
